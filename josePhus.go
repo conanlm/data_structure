38f079a49879dd51274054cc931651096f70cf7f
ivan <ivan@test.com>
2017-08-15 18:03:46 +0800

    提交

---
+++ josePhus.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Node struct {
+	num   int
+	next *Node
+}
+
+func createList(size int) *Node {
+	if size < 0 {
+		os.Exit(-1)
+	}
+	node := new(Node)
+	node.num = 1
+	q := node
+	for i := 2; i <= size; i++ {
+		list := new(Node)
+		list.num = i
+		node.next = list
+		node = list
+	}
+	node.next = q
+	return node
+}
+
+func empty(list *Node) bool {
+	if list.next == list {
+		return true
+	}
+	return false
+}
+
+func traverse(list *Node) {
+	if empty(list) {
+		return
+	}
+	for p := list.next; p != list; p = p.next {
+		fmt.Printf("%5d", p.num)
+	}
+	fmt.Printf("%5d", list.num)
+	fmt.Println()
+}
+
+func length(list *Node) int {
+	i := 1
+	for p := list.next; p != list; p = p.next {
+		i++
+	}
+	return i
+}
+
+func jose(list *Node, n int) {
+	node := list.next
+	count := 1
+	for 1 < length(node) {
+		for i := 1; i < n-1; i++ {
+			node = node.next
+		}
+		kill := node.next
+		fmt.Printf("第%d个出局的人为：%3d号\n", count, kill.num)
+		node.next = kill.next
+		node = kill.next
+		kill = nil
+		count++
+	}
+	fmt.Println("最后获胜的是: ", node.num)
+}
+
+func main() {
+	list := createList(41)
+	traverse(list)
+	jose(list, 3)
+}
